ee8afef3b8fd57146009a72d5ee5a052e28e82c1
kaymohh <thrilltim@gmail.com>
2025-06-16 17:29:19 +0100

    toggle like button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-
-
-import React, { Component } from 'react';
-import Movies from './components/movies';
+import React, { Component } from "react";
+import Movies from "./components/movies";
 
 class App extends Component {
-  // state = {  } 
-  render() { 
+  // state = {  }
+  render() {
     return (
-      <main className='container'>
+      <main className="container">
         <Movies />
-
-  
-
       </main>
     );
   }
 }
- 
+
 export default App;
--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -7,134 +7,135 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 6,
     dailyRentalRate: 2.5,
-    publishDate: "2018-01-03T19:04:28.809Z"
+    publishDate: "2018-01-03T19:04:28.809Z",
   },
   {
     _id: "5b21ca3eeb7f6fbccd471816",
     title: "Die Hard",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 5,
-    dailyRentalRate: 2.5
+    dailyRentalRate: 2.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
     title: "Get Out",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 8,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471819",
     title: "Trip to Italy",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181a",
     title: "Airplane",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
     title: "Wedding Crashers",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
     title: "Gone Girl",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 7,
-    dailyRentalRate: 4.5
+    dailyRentalRate: 4.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181f",
     title: "The Sixth Sense",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 4,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821",
     title: "The Avengers",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471822",
     title: "Mission Impossible",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 5,
-    dailyRentalRate: 3.0
+    dailyRentalRate: 3.0,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471823",
     title: "John Wick",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 4,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: true,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471824",
     title: "Split",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 5,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471825",
     title: "Prisoners",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 6,
-    dailyRentalRate: 4.0
+    dailyRentalRate: 4.0,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471826",
     title: "Superbad",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 8,
-    dailyRentalRate: 3.0
+    dailyRentalRate: 3.0,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471827",
     title: "Step Brothers",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 9,
-    dailyRentalRate: 2.5
+    dailyRentalRate: 2.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471828",
     title: "Logan",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 5,
-    dailyRentalRate: 4.0
+    dailyRentalRate: 4.0,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471829",
     title: "Arrival",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 5,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47182a",
     title: "Paranormal Activity",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 6,
-    dailyRentalRate: 2.5
+    dailyRentalRate: 2.5,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47182b",
     title: "Spy",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.0
-  }
+    dailyRentalRate: 3.0,
+  },
 ];
 
 export function getMovies() {
@@ -142,13 +143,13 @@ export function getMovies() {
 }
 
 export function getMovie(id) {
-  return movies.find(m => m._id === id);
+  return movies.find((m) => m._id === id);
 }
 
 export function saveMovie(movie) {
-  let movieInDb = movies.find(m => m._id === movie._id) || {};
+  let movieInDb = movies.find((m) => m._id === movie._id) || {};
   movieInDb.name = movie.name;
-  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
+  movieInDb.genre = genresAPI.genres.find((g) => g._id === movie.genreId);
   movieInDb.numberInStock = movie.numberInStock;
   movieInDb.dailyRentalRate = movie.dailyRentalRate;
 
@@ -161,7 +162,7 @@ export function saveMovie(movie) {
 }
 
 export function deleteMovie(id) {
-  let movieInDb = movies.find(m => m._id === id);
+  let movieInDb = movies.find((m) => m._id === id);
   movies.splice(movies.indexOf(movieInDb), 1);
   return movieInDb;
 }
